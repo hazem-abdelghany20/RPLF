bbaf0c0ff9503fc0df08fe71bea93be1b49ee39c
ahmedyasser07 <ahmedtaweel.65@gmail.com>
2024-04-13 22:13:21 +0200

    added actions to images in image slider block

--- server/src/blocks/Image_Slider/Config.ts
+++ server/src/blocks/Image_Slider/Config.ts
@@ -1,5 +1,8 @@
 import { Block } from 'payload/types';
 import BlockMedia from '../../collections/BlockMedia';
+type Data = Record<string, unknown>;
+
+const customURLCondition = (_: Data, siblings: Data): boolean => siblings.type === 'custom';
 
 export const ImageSlider: Block = {
   slug: 'image_slider',
@@ -25,7 +28,85 @@ export const ImageSlider: Block = {
         {
           name:'alt',
           type:'text'
-        }
+        },
+        {
+          type: 'row',
+          fields: [
+           
+            // {
+            //   name: 'type',
+            //   label: 'Image Actions',
+            //   type: 'radio',
+            //   defaultValue: 'page',
+            //   options: [
+            //     {
+            //       label: 'Page',
+            //       value: 'page',
+            //     },
+            //     {
+            //       label: 'Custom URL',
+            //       value: 'custom',
+            //     },
+            //   ],
+            //   admin: {
+            //     width: '50%',
+            //     layout: 'horizontal',
+            //   },
+            // },
+            {
+              name: 'type',
+              label: 'Image Actions',
+              type: 'select',  // Changed from 'radio' to 'select'
+              defaultValue: '',  // Default to none
+              options: [
+                {
+                  label: 'None',  // Allows for no selection
+                  value: '',
+                },
+                {
+                  label: 'Page',
+                  value: 'page',
+                },
+                {
+                  label: 'Custom URL',
+                  value: 'custom',
+                },
+              ],
+              admin: {
+                width: '50%',
+               // layout: 'horizontal',
+              },
+            },
+          ],
+        },
+        {
+          name: 'page',
+          label: 'Page to link to',
+          type: 'relationship',
+          relationTo: 'pages',
+          required: true,
+          admin: {
+            condition: (_: Data, siblings: Data): boolean => siblings.type === 'page',
+          },
+        },
+        {
+          name: 'url',
+          label: 'Button URL',
+          type: 'text',
+          required: true,
+          admin: {
+            condition: customURLCondition,
+          },
+        },
+        {
+          name: 'newTab',
+          type: 'checkbox',
+          label: 'Open in new tab',
+          required: true,
+          admin: {
+            condition: customURLCondition,
+          },
+        },
       ]
     },
   ],
